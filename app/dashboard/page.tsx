ed14aa905c06f5ef8a6d57bc7d5726702068c18b
Muhammad <24100113@lums.edu.pk>
2024-02-06 02:45:27 +0500

    code files

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import {lusitana} from '../ui/fonts';
+
+export default function Page() {
+    return <h1 className= {`${lusitana.className} antialiased`}>Dashboard Page</h1>;
+  }
